713b0c67705c18f008efb86f05923de03387862d
User <user@users-macbook.home>
2018-10-27 02:02:35 +0100

    lesson-01

---
+++ coder-swag/View/CategoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryViewCell.swift
+//  coder-swag
+//
+//  Created by User on 26/10/2018.
+//  Copyright © 2018 User. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
