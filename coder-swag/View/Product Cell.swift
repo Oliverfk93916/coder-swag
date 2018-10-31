7506f8742345f8215365507d2b588f4b3883e67c
User <user@users-macbook.home>
2018-10-31 18:27:37 +0000

    End of lesson-03 added ProductVC

---
+++ coder-swag/View/Product Cell.swift
@@ -0,0 +1,15 @@
+//
+//  Product Cell.swift
+//  coder-swag
+//
+//  Created by User on 31/10/2018.
+//  Copyright © 2018 User. All rights reserved.
+//
+
+import UIKit
+
+class Product_Cell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
