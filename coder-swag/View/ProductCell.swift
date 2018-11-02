0673c70bca20346f7f24ba596f9f4f52430b5744
User <user@users-macbook.home>
2018-11-02 16:35:48 +0000

    app complete

--- coder-swag/View/ProductCell.swift
+++ coder-swag/View/ProductCell.swift
@@ -8,8 +8,17 @@
 
 import UIKit
 
-class Product_Cell: UICollectionViewCell {
+class ProductCell: UICollectionViewCell {
+    
     @IBOutlet weak var productImage: UIImageView!
     @IBOutlet weak var productTitle: UILabel!
     @IBOutlet weak var productPrice: UILabel!
+    
+
+func updateViews(product: Product) {
+    productImage.image = UIImage(named: product.imageName)
+    productTitle.text = product.title
+    productPrice.text = product.price
+    }
 }
+
